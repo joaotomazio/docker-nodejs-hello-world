2d55d505332369487b7f13a02aef1776368f3144
João Tomázio <jdtomazio@gmail.com>
2020-02-12 15:35:11 +0000

    added some text

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World from a Node.js server on a Docker image!'))
+app.get('/', (req, res) => res.send('Hello World from a Node.js server on a Docker image! I am listening on port 8080'))
 
 app.listen(port, () => console.log(`Hello World app listening on port ${port}!`))
